1dd5f8bbb6ded8284cb477caaca6a44e09770986
doridoro <dorothea.reher@gmail.com>
2023-02-21 18:43:28 +0100

    add methods and modified the staticmethod

--- chess.py
+++ chess.py
@@ -1,3 +1,6 @@
+import json
+import random
+import string
 
 from datetime import datetime
 
@@ -5,78 +8,103 @@ from datetime import datetime
 # models
 class Player:
 
-	def __init__(self, player_id, first_name, last_name, birth_date):
-		self.player_id = player_id
-		self.first_name = first_name
-		self.last_name = last_name
-		self.birth_date = birth_date
+    def __init__(self, player_id, first_name, last_name, birth_date):
+        self.player_id = player_id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.birth_date = birth_date
 
-		self.rank = 0
-		self.score = 0.0
-		self.played_against = []
+        self.rank = 0
+        self.score = 0.0
+        self.played_against = []  # add the player_id
+        self.played_tournaments = []  # add the tournament_id
 
-	def __str__(self):
-		return f"{self.first_name} {self.last_name}"
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
 
-	@staticmethod
-	def create_player():
-		players = []
+    @staticmethod
+    def create_player():
+        player_id = str(input("Enter the ID of the player (example: AB12345): "))
+        first_name = str(input("Enter the first name of the player: "))
+        last_name = str(input(f"Enter {first_name}'s last name: "))
+        birthday = str(input(f"Enter the birth date of {first_name} {last_name} (yyyy-mm-dd): "))
+        birth_date = datetime.strptime(birthday, "%Y-%m-%d")
+        print(f"You have just created this player: [id]: {player_id}, [name]: {first_name} {last_name}, [birthday]: "
+              f"{birth_date}")
 
-		player_id = str(input("Enter the ID of the player (example: AB12345): "))
-		# check if the player_id is 2 letters and 5 numbers
-		first_name = str(input("Enter the first name of the player: "))
-		last_name = str(input(f"Enter {first_name}'s last name: "))
-		birthday = str(input(f"Enter the birth date of {first_name} {last_name} (yyyy-mm-dd): "))
-		birth_date = datetime.strptime(birthday, "%Y-%m-%d")
+        return {"player_id": player_id, "first_name": first_name, "last_name": last_name, "birth_date": birth_date}
 
-		new_player = Player(player_id, first_name, last_name, birth_date)
-		players.append(new_player)
+    def save_player(self, list_of_players):
+        pass
 
-		return players
+    # x = { 'first_name': 'John', 'last_name': 'Doe'}
+    # y = json.dumps(x)
+    # save player in JSON data file
+    # save players in data/players/{name}.json
 
-# create player_id for created player
-# connect a player/s with a Tournament, add_player_to_tournament
-# save player in JSON data file
+    def create_pairs(self):
+        pass
+
+    # create pairs to play against
+    # check if they played already against each other
+    # update played_against, add the players_id
+    # update played_tournaments, add the tournament_id
+
+    def check_opponents(self):
+        # check if the player played already against the opponent
+        pass
+
+    def calculate_score(self):
+        pass
+
+    def calculate_rank(self):
+        pass
 
 
 class Tournament:
 
-	def __init__(
-			self, name, city, start_date, end_date, list_rounds, comments, rounds=4):
-		self.name = name
-		self.city = city
-		self.start_date = start_date
-		self.end_date = end_date
-		self.list_rounds = list_rounds
-		self.comments = comments
-		self.rounds = rounds
+    def __init__(
+            self, name, city, start_date, end_date, list_rounds, comments, rounds=4):
+        self.name = name
+        self.city = city
+        self.start_date = start_date
+        self.end_date = end_date
+        self.list_rounds = list_rounds
+        self.comments = comments
+        self.rounds = rounds
 
-		self.list_of_players = []
-		self.tournament_id = ""
-		self.current_round = 0
-		self.match_list = []
+        self.list_of_players = []  # add the players_id
+        self.tournament_id = ""
+        self.current_round = 0
 
-	@staticmethod
-	def create_tournament():
-		tournaments = []
+    @staticmethod
+    def create_tournament():
+        tournament_id = int(input("Please enter the ID of the Tournament: "))
+        name = str(input("Enter the name of the Chess Tournament: "))
+        city = str(input("Enter the location of the Chess Tournament: "))
+        s_date = str(input("When does the Chess Tournament starts? (yyyy-mm-dd): "))
+        start_date = datetime.strptime(s_date, "%Y-%m-%d")
+        e_date = str(input("Please enter the end date of the Chess Tournament (yyyy-mm-dd): "))
+        end_date = datetime.strptime(e_date, "%Y-%m-%d")
+        rounds = int(input("How many rounds are possible for this Chess Tournament? "))
+        comments = str(input("Do you have any comments concerning the Chess Tournament? "))
+        print(f"You have just created this tournament: [name]: {name}, [city]: {city}, [start date]: "
+              f"{start_date}, [end date]: {end_date}, [rounds]: {rounds}, [comments]: {comments}")
 
-		name = str(input("Enter the name of the Chess Tournament: "))
-		city = str(input("Enter the location of the Chess Tournament: "))
-		s_date = str(input("When does the Chess Tournament starts? (yyyy-mm-dd): "))
-		start_date = datetime.strptime(s_date, "%Y-%m-%d")
-		e_date = str(input("Please enter the end date of the Chess Tournament (yyyy-mm-dd): "))
-		end_date = datetime.strptime(e_date, "%Y-%m-%d")
-		rounds = int(input("How many rounds are possible for this Chess Tournament? "))
-		comments = str(input("Do you have any comments concerning the Chess Tournament? "))
+        return {"tournament_id": tournament_id, "name": name, "city": city, "start_date": start_date,
+                "end_date": end_date, "rounds": rounds, "comments": comments}
 
-		new_tournament = Tournament(name, city, start_date, end_date, rounds, comments)
-		tournaments.append(new_tournament)
+    def save_tournament(self):
+        pass
+    # take the dict from create_tournament and save it
+    # save in json in data/tournaments/{name}.json
 
-	def get_tournament(self):
-		pass
+    def get_tournaments(self, name_of_tournament):
+        pass
+    # get a tournament by tournament_id from json file
+    # update the list_of_players of tournament, add the players_id
 
-	# def get_tournament
-	# def update_tournament
-	# def update_match_list
-	# def shuffle_players including the first match
 
+    def calculate_current_round(self):
+        pass
+# update current_round, add 1
