6f7c668c5dc340160c79bf33c666315425026804
doridoro <dorothea.reher@gmail.com>
2023-03-03 16:17:09 +0100

    modified Tournament, changed creation of tournament, started with next step

--- model/tournament.py
+++ model/tournament.py
@@ -5,15 +5,15 @@ from unidecode import unidecode
 class Tournament:
 
     def __init__(
-            self, name, city, start_date, end_date, list_tours, comments, rounds=4):
+            self, name, city, start_date, end_date, comments, rounds=4):
         self.name = name
         self.city = city
         self.start_date = start_date
         self.end_date = end_date
-        self.list_tours = list_tours
         self.comments = comments
         self.rounds = rounds
 
+        self.list_tours = []
         self.list_of_players = []  # add the players_id
         self.tournament_id = ""
         self.current_round = 0
--- views/tournament.py
+++ views/tournament.py
@@ -4,12 +4,11 @@ from model.tournament import Tournament
 
 
 def create_tournament():
-    tournament_id = int(input("Please enter the ID of the Tournament: "))
+    tournament_id = int(input("Please enter the ID of the Tournament (example: 1234): "))
     name = str(input("Enter the name of the Chess Tournament: "))
     city = str(input("Enter the location of the Chess Tournament: "))
     start_date = str(input("When does the Chess Tournament starts? (dd-mm-yyyy): "))
     end_date = str(input("Please enter the end date of the Chess Tournament (dd-mm-yyyy): "))
-    list_tours = int(input("Question: "))
     rounds = int(input("How many rounds are possible for this Chess Tournament? "))
     comments = str(input("Do you have any comments concerning the Chess Tournament? "))
     print()
@@ -18,16 +17,14 @@ def create_tournament():
     print(f"  [name]: {name}")
     print(f"  [city]: {city}")
     print(f"  [start date]: {start_date} and [end date]: {end_date}")
-    print(f"  [list_tours]: {list_tours}")
     print(f"  [rounds]: {rounds}")
     print(f"  [comments]: {comments}", end="\n\n")
 
-    new_tournament = Tournament(name, city, start_date, end_date, list_tours, comments, rounds)
-    data = {"tournament_id": new_tournament.tournament_id, "name": new_tournament.name,
+    new_tournament = Tournament(name, city, start_date, end_date, comments, rounds)
+    data = {"tournament_id": tournament_id, "name": new_tournament.name,
             "city": new_tournament.city, "start_date": new_tournament.start_date,
-            "end_date": new_tournament.end_date, "list_tours": new_tournament.list_tours,
-            "rounds": new_tournament.rounds, "comments": new_tournament.comments,
-            "list_of_players": [], "current_round": 0}
+            "end_date": new_tournament.end_date, "rounds": new_tournament.rounds, "comments": new_tournament.comments,
+            "list_tours": [], "list_of_players": [], "current_round": 0}
 
     db = TinyDB(f'data/tournaments/tournaments.json', indent=4)
     db.insert(data)
